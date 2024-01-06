acd6ba0522647c79bbab8eabf3ff3f332c26b70f
raphael <raphael.aldaz@gmail.com>
2024-01-06 17:06:36 +0100

    Initialization project

---
+++ src/main/java/ar/BeatBuddyBackApplication.java
@@ -0,0 +1,13 @@
+package ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatBuddyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatBuddyBackApplication.class, args);
+	}
+
+}
